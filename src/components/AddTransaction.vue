d37dc3b847550a801d7cf7761205d26fa82aa886
Kashsan <kashsan_jp@yahoo.co.jp>
2025-01-26 21:44:00 +0900

    First Commit

---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,17 @@
+<template>
+    <h3>Add New Transaction</h3>
+    <form class="form">
+        <div class="form-control">
+            <label for="text">Text</label>
+            <input type="text" id="text" placeholder="Enter Text..."/>
+        </div>
+        <div class="form-control">
+            <label for="amount">
+                Amount<br />
+                (negative - expense, positive - income)
+            </label>
+            <input type="number" id="amount" placeholder="Enter Amount..."/>
+        </div>
+        <button class="btn">Add transaction</button>
+    </form>
+</template>
